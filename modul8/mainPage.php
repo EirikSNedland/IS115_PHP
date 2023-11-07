9d1e07f48c9e00341d1de33fd57451d11d381b92
Eirik <eiriksnedland@gmail.com>
2023-11-07 13:52:05 +0100

    access control på mainPage

--- modul8/mainPage.php
+++ modul8/mainPage.php
@@ -8,11 +8,26 @@
 <body>
     <?php
         session_start();
-        
+
+        if (!isset($_SESSION["user"])) {
+            header("location: index.php");
+            exit;
+        }
+
         echo $_SESSION["user"]["email"] . "<br>";
         echo $_SESSION["user"]["fname"] . "<br>";
         echo $_SESSION["user"]["lname"] . "<br>";
         echo $_SESSION["user"]["mobil"] . "<br>";
+
+        if(isset($_POST["logOut"])){
+            unset($_SESSION["user"]);
+            header("location: index.php");
+            exit;
+        }
     ?>
+
+    <form method="POST">
+        <input type="submit" name="logOut" value="Logg ut">
+    </form>
 </body>
 </html>
