f5d3bef32846c0eea6888394ca0ea8ff6ebef9df
Eirik <eiriksnedland@gmail.com>
2023-11-17 15:24:56 +0100

    Oppgave4 modul9 gjorde form om til linker pga krav

--- modul9/oppgave4.php
+++ modul9/oppgave4.php
@@ -9,29 +9,28 @@
 <body>
     <a href="index.php">Go Back</a><br> 
     <form>
-        <select name="fileToDownload">
-            <option value="lastMegNed.pdf">lastMegNed</option>
-            <option value="pannekake.pdf">pannekake</option>
-            <option value="Innlevering9_fakturamal.pdf">faktura mal</option>
-
-        </select>
-        <input type="submit" name="download" value="Last ned PDF">
+    <ul>
+        <li><a href="oppgave4.php?file=lastMegNed.pdf">Last Meg Ned</a></li>
+        <li><a href="oppgave4.php?file=pannekake.pdf">pannekake</a></li>
+        <li><a href="oppgave4.php?file=Innlevering9_fakturamal.pdf">faktura mal</a></li>
+    </ul>
     </form>
     <?php 
-        if(isset($_GET["download"])){
-            $filePath = "./katalog/ignoreMe/" . sanetize($_GET["fileToDownload"]);
-            echo $filePath;
+        if(isset($_GET["file"])){
+            $fileToDownload = $_GET["file"];
+            $filePath = "./katalog/ignoreMe/" . sanitize($fileToDownload);
+
             if(file_exists($filePath)){
                 echo "<br>fant fil<br>";
                 header("Content-Description: File Transfer");
                 header("Content-Type: application/pdf");
                 header("Content-Length: " . filesize($filePath));
                 header("Content-Transfer-Encoding: Binary");
-                #Bruker $_GET["fileToDownload"] istedenfor $filePath for at navnet til filen ikke skal si hvor den er lagret
-                header("Content-Disposition: attachment; filename=\"" . $_GET["fileToDownload"] ."\" "); 
+                header("Content-Disposition: attachment; filename=\"" . $_GET["file"] ."\" "); 
                 header("Pragma: public");
 
                 readfile($filePath);
+                exit;
             } else {
                 echo "Noe gikk gale";
             }
@@ -40,7 +39,7 @@
 
 
 <?php 
-    function sanetize(string $text){
+    function sanitize(string $text){
         $text = strip_tags($text);
         $text = htmlspecialchars($text);
         return $text;
